8f0cadc39693493b40e8b47fd3ad23255b646dea
Tanner Summers <tgsummers@cpp.edu>
2017-02-18 23:36:42 -0800

    - Backup

---
+++ handler.js
@@ -0,0 +1,27 @@
+'use strict';
+
+let request = require('request');
+let AWS = require('aws-sdk');
+
+AWS.config.update({
+    region: "us-west-1"
+});
+
+let db = new AWS.DynamoDB.DocumentClient();
+let table = "BroncoExpress";
+
+module.exports.updateInfo = (event, context, callback) =>
+{
+    // Contains incoming request data (e.g., query params, headers and more)
+    console.log(event);
+
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify({
+            message: 'Shuttle data updated!'
+        }),
+    };
+
+    // send back to page
+    callback(null, response);
+};
